7441201a6d3c5282cec3e12e2344703a2e357f07
meustar <meustar829@gmail.com>
2024-11-14 15:16:27 +0900

    테라폼으로 EC2 1개 생성

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "dev"
+}
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+variable "nickname" {
+  description = "nickname"
+  default     = "meustar829"
+}
